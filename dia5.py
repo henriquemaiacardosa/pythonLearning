c7102f04f8971a5a90f77d71cfeee1faa48e0053
Henrique Maia Cardosa <132239829+henriquemaiacardosa@users.noreply.github.com>
2025-09-02 16:51:36 -0300

    Add files via upload

--- dia5.py
+++ dia5.py
@@ -138,11 +138,98 @@ print("A soma é", soma)
 # for i in range(15, n+1,15):
 #     print(i)
 
-# while 
-n = int(input("Type a number: "))
-i = 15
-while i <= n:
-    print(i)
-    i += 15
+# # while 
+# n = int(input("Type a number: "))
+# i = 15
+# while i <= n:
+#     print(i)
+#     i += 15
     
+# Peça ao usuário um número inteiro positivo N e calcule a soma de 
+# todos os números pares de 1 até N.
+
+# n = int(input("Digite um número: "))
+# soma = 0
+
+# for i in range(2, n+1, 2):
+#     soma +=i
+#     print(i)
+# print(f"A soma dos pares até {n} é: ",soma)
+
+# n = int(input("Digite um número: "))
+# i = 0
+# soma = 0
+
+# while i < n:
+#     i += 2
+#     soma += i
+# print(soma)
+    
+#Faça um programa que peça uma nota, entre zero e dez. 
+# Mostre uma mensagem caso o valor seja inválido e 
+# continue pedindo até que o usuário informe um valor válido.
+
+# nota = float(input("Digite uma nota: "))
+# while nota < 0 or nota > 10:
+#     nota = int(input("Digite novamente a nota: "))
+# print("A nota é:",nota)
+
+# Faça um programa que leia um nome de usuário e a sua senha 
+# e não aceite a senha igual ao nome do usuário, 
+# mostrando uma mensagem de erro e voltando a pedir as informações.
+
+# nome = str(input("Digite o seu login: "))
+# senha = str(input("Digite o sua senha: "))
+
+# while senha == nome:
+#     senha = str(input("A senha não pode ser igual ao login, digite outra senha: "))
+# print("Login",nome)
+# print("Senha",senha)
+
+# Faça um programa que leia e valide as seguintes informações:
+# Nome: maior que 3 caracteres;
+# Idade: entre 0 e 150;
+# Salário: maior que zero;
+# Sexo: 'f' ou 'm';
+# Estado Civil: 's', 'c', 'v', 'd';
+
+# nome = str(input("Digite um nome maior que 3 caracteres: "))
+# while len(nome) < 3:
+#         nome = str(input("Digite um nome maior que 3 caracteres: "))
+# print(nome)
+
+# idade = int(input("Digite uma idade: "))
+# while (idade > 150) or (idade < 0):
+#         idade = int(input("Digite uma idade válida: "))
+# print(idade)
+
+# salario = int(input("Digite um salário: "))
+# while salario < 0:
+#     salario = int(input("Digite um salário válido: "))
+# print(salario)
+
+# sexo = str(input("Digite o sexo F/M: "))
+# gen = ["F","f","M","m"]
+# while sexo not in gen:
+#     sexo = str(input("Digite um sexo válido: "))
+# print(sexo)
+
+estadoCivil = str(input("Digite o seu estado S/C/V/D: "))
+estciv = ["S","s","C","c","V","v","D","d"]
+while estadoCivil not in estciv:
+    estadoCivil = str(input("Digite um sexo válido: "))
+if estadoCivil == "S" or estadoCivil == "s":
+    print("Solteiro") 
+elif estadoCivil == "C" or estadoCivil == "c":
+    print("Casado") 
+elif estadoCivil == "V" or estadoCivil == "v":
+    print("Viuvo") 
+elif estadoCivil == "D" or estadoCivil == "d":
+    print("Divorciado") 
+
+
+
+
+
+
     
