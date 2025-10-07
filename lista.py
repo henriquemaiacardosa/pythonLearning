d80f4b7644d4e10a89b38dff551571d0512dc016
Henrique Maia Cardosa <132239829+henriquemaiacardosa@users.noreply.github.com>
2025-10-07 10:13:23 -0300

    Add files via upload

--- lista.py
+++ lista.py
@@ -146,7 +146,222 @@
 # Crie uma função chamada mostrar_mensagem() que:
 # Mostre a mensagem: "Bem-vindo ao mundo das funções!"
 
-def mostrar_mensagem():
-    print("Bem-vindo ao mundo das funções!")
+# def mostrar_mensagem():
+#     print("Bem-vindo ao mundo das funções!")
 
-mostrar_mensagem()
+# mostrar_mensagem()
+
+#Peça ao usuário dois números inteiros: o início e o fim.
+#Depois, faça uma contagem do início até o fim, 
+# um número por linha, usando for
+
+# n1 = int(input("Digite o início: "))
+# n2 = int(input("Digite o fim: "))
+# if n1 < n2:
+#     for i in range(n1, n2+1):
+#         print(i)
+# else:
+#     for y in range(n1, n2-1, -1):
+#         print(y)
+
+# n1 = int(input("Digite o início: "))
+# n2 = int(input("Digite o fim: "))
+# i = n1
+# if n1 < n2:
+#    while i < n2:
+#         print(i)
+#         i +=1
+# else:
+#     while i > n2:
+#         print(i)
+#         i -= 1
+
+
+# Crie um programa que:
+#     Peça números inteiros ao usuário, um por vez
+#     Pare quando o usuário digitar 0
+#     No final, mostre:
+#     A soma apenas dos números pares
+#     A quantidade de pares digitados
+    
+# nP = int(input("Digite um número (Digite 0, para parar): "))
+# soma = 0
+# i = 0
+
+# while nP != 0:
+#     if nP % 2 == 0:
+#         soma += nP
+#         i += 1
+#     nP = int(input("Digite um número (Digite 0, para parar): "))
+
+# if nP == 0:
+#     print("A soma dos pares é: ",soma)    
+#     print("A quantidade de pares digitados é: ",i)    
+
+
+# Peça números ao usuário até ele digitar 0
+#     Ao final, mostre:
+#     O maior número digitado
+#     O menor número digitado
+#     (Sem contar o 0!)
+
+# numero = float(input("Digite um número (Digite 0 para parar): "))
+# lista_numeros = []
+
+# while numero != 0:
+#     lista_numeros.append(numero)
+#     numero = float(input("Digite um número (Digite 0 para parar): "))
+    
+# # Se o primeiro número for 0, da erro.
+# print("O maior número digitado é: ",max(lista_numeros))
+# print("O menor número digitado é: ",min(lista_numeros))
+
+
+# Peça números inteiros ao usuário até ele digitar 0
+#     No final, mostre:
+#     Quantos números pares foram digitados
+#     Quantos números ímpares foram digitados
+
+# # Entrada
+# entrada = float(input("Digite um número (Digita 0 para parar): "))
+# listaDosNumerosDigitados = []
+# contador_par = 0
+# contador_impar = 0
+
+# # Processamento 
+# while entrada != 0:
+#     if entrada % 2 == 0:
+#         contador_par += 1
+#         listaDosNumerosDigitados.append(entrada)
+#         entrada = float(input("Digite um número (Digita 0 para parar): "))
+#     else:
+#         contador_impar += 1
+#         listaDosNumerosDigitados.append(entrada)
+#         entrada = float(input("Digite um número (Digita 0 para parar): "))
+
+# # Saida
+# print("A quantidade de pares é: ", contador_par)
+# print("A quantidade de impares é: ", contador_impar)
+    
+# numero = int(input("Digite um número: "))   
+# lista = []
+
+# while numero >= 0:
+#     numero = int(input("Digite um número: "))           
+#     lista.append(numero)
+#     if len(lista) == 2:
+#         diferenca = lista[0] - lista[1]
+#         print(f"A diferença entre {lista[0]} e {lista[1]} é {diferenca}")
+#         lista.clear()
+    
+        
+# Você vai simular uma caminhada numa montanha. 
+    # O programa começa com um "altitude" igual a 0. 
+    # A cada passo, o usuário informa se subiu (S) ou desceu (D) 1 metro. 
+        # O programa atualiza a altitude e exibe o valor atual.
+            # O programa para quando o usuário digitar P (de "Parar").
+    # No final, o programa mostra:
+        # A maior altitude atingida
+        # A menor altitude atingida        
+
+# altitude = 0
+# max = 0
+# min = 0
+
+# print("Controle a Montanha Russa (D para descer/ S para subir e P para Parar)")
+# passo = ""
+# while passo != "P" and passo != "p":
+#     passo = str(input("Subir/Descer/Parar: "))
+
+#     if passo == "S" or passo == "s": 
+#         altitude += 1
+#         if altitude > max:
+#             max = altitude
+    
+#     elif passo == "D" or passo == "d": 
+#         altitude -= 1
+#         if altitude < min:
+#             min = altitude
+
+#     elif passo == "P" or passo == "p":
+#         break
+
+#     else:
+#         print("Movimento inválido")
+#         continue
+# print("A maior altitude atingida",max)
+# print("A menor altitude atingida",min)
+
+##########################################
+        
+#Escreva um programa que pede um número n e soma todos os números de 1 até n.
+
+# n = int(input("Digite um número: "))
+# soma = 0
+
+# for i in range(1, n+1):
+#     print(i, end=" + ")
+#     soma += i
+# print("=",soma)
+
+# Peça um número n, e mostre todos os números pares de 0 até n.
+
+# n = int(input("Digite um número: "))
+
+# for i in range(0, n, 2):
+#     print(i, end=" ")
+
+#O programa pede números até o usuário digitar 0.
+#Quando ele digita 0, o programa mostra quantos números foram digitados (sem contar o zero).
+
+# n = int(input("Digite um número: "))
+# contador = 0
+
+# while n != 0:
+#     n = int(input("Digite um número: "))
+#     contador +=1
+# print("Foram digitados:",contador,"números")
+
+# O programa pede números ao usuário até ele digitar -1.
+#No final, o programa mostra o maior e o menor número que o usuário digitou.
+
+# n = int(input("Digite um número (Para parar, digite -1): "))
+
+# if n == -1:
+#     print("Nenhum número válido foi digitado.")
+# else:
+#     maior = n
+#     menor = n
+
+#     while True:
+#         n = int(input("Digite um número (Para parar, digite -1): "))
+#         if n == -1:
+#             break
+#         if n > maior:
+#             maior = n
+#         if n < menor:
+#             menor = n
+
+#     print("O maior número digitado foi:", maior)
+#     print("O menor número digitado foi:", menor)
+
+
+# contador = 0
+# soma = 0
+# ultimo = 0
+
+# while True:
+#     numero = int(input("Digite um número: "))
+#     contador += 1
+#     soma += numero
+#     media = soma / contador
+#     ultimo = numero
+#     if media > 20:
+#         print("Último número digitado: ",numero)
+#         print("Media: ",media)
+#         break
+#     else:
+#         ultimo = numero
+
+
+
---
+++ projetinho.py
@@ -0,0 +1,30 @@
+# lista_de_notas = []
+# lista_de_alunos = []
+
+# print("Cálcule a média da sala: ")
+# quantidade_alunos = int(input("Quantos alunos são? "))
+
+# for i in range (1, quantidade_alunos+1):
+#     nota = int(input(f"Digite a nota do Aluno {i} : "))
+#     lista_de_notas.append(nota)
+#     media = sum(lista_de_notas) / len(lista_de_notas)
+
+# for i, nota in enumerate(lista_de_notas):
+#     print(f"Aluno {i+1}: {nota}")
+#     aluno = i
+#     lista_de_alunos.append(aluno)
+# print("A média da sala é: ", media)
+
+# nomeando_alunos = str(input("Gostaria de nomear os alunos? (S/N): "))
+
+# if nomeando_alunos == "S" or nomeando_alunos == "s":
+#     numero_aluno = int(input("Escolha qual aluno nomear: "))
+#     nome_aluno = str(input("Digite um nome: "))
+#     lista_de_alunos[numero_aluno - 1] = nome_aluno
+    
+#     for aluno in enumerate(lista_de_alunos):
+
+#         print(f"{nome_aluno}: {nota}")
+
+
+    
